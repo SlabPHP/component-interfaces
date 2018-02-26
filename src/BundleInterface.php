99b077d54c4955aa1655b01a8724ccc85d2e5c4f
Eric Salerno <salernolabs@gmail.com>
2018-02-26 18:00:12 -0500

    Add BundleInterface

---
+++ src/BundleInterface.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Bundle Interface
+ *
+ * @package Slab
+ * @subpackage Components
+ * @author Eric
+ */
+namespace Slab\Components;
+
+interface BundleInterface
+{
+    /**
+     * BundleInterface constructor.
+     */
+    public function __construct($namespace);
+
+    /**
+     * @return string
+     */
+    public function getSourceDirectory();
+
+    /**
+     * @return string
+     */
+    public function getPackageDirectory();
+
+    /**
+     * @return string
+     */
+    public function getViewDirectory();
+
+    /**
+     * @return string
+     */
+    public function getConfigDirectory();
+
+    /**
+     * @return string
+     */
+    public function getResourceDirectory();
+
+    /**
+     * @return string
+     */
+    public function getNamespace();
+}
