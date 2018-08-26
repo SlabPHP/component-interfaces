3be13d3edf4072045928dc6bdfb629d9784adebf
Eric Salerno <salernolabs@gmail.com>
2018-08-26 14:58:39 -0400

    Fix mock test assertions.

--- tests/VerifyMocksTest.php
+++ tests/VerifyMocksTest.php
@@ -16,18 +16,39 @@ class VerifyMocksTest extends \PHPUnit\Framework\TestCase
     public function testMockCreations()
     {
         $system = new Mocks\System();
+        $this->assertNotEmpty($system);
+
         $log = new Mocks\Log();
+        $this->assertNotEmpty($log);
+
         $input = new Mocks\Input();
+        $this->assertNotEmpty($input);
+
         $bundle = new Mocks\Bundle([]);
+        $this->assertNotEmpty($bundle);
+
+        $bundleStack = new Mocks\BundleStack();
+        $this->assertNotEmpty($bundleStack);
 
         $router = new Mocks\Router\Router();
+        $this->assertNotEmpty($router);
+
         $route = new Mocks\Router\Route();
+        $this->assertNotEmpty($route);
+
         $controller = new Mocks\Router\Controller();
+        $this->assertNotEmpty($controller);
 
         $databaseDriver = new Mocks\Database\Driver();
+        $this->assertNotEmpty($databaseDriver);
+
         $databaseProvider = new Mocks\Database\Provider();
+        $this->assertNotEmpty($databaseProvider);
+
         $databaseResponse = new Mocks\Database\Response();
+        $this->assertNotEmpty($databaseResponse);
 
         $cacheDriver = new Mocks\Cache\Driver('provider');
+        $this->assertNotEmpty($cacheDriver);
     }
 }
